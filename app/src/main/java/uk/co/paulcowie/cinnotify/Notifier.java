cc1c7b7916efcb2e6d6fb0d972f98c4f24197084
Paul Cowie <2082442C@student.gla.ac.uk>
2015-08-18 19:32:58 +0100

    Move notifier to seperate class

---
+++ app/src/main/java/uk/co/paulcowie/cinnotify/Notifier.java
@@ -0,0 +1,37 @@
+package uk.co.paulcowie.cinnotify;
+
+import android.app.NotificationManager;
+import android.content.Context;
+import android.support.v4.app.NotificationCompat;
+import android.view.View;
+
+/**
+ * Wrapper around NotificationCompat.Builder which makes it easier to construct simple
+ * notifications for testing
+ */
+public class Notifier {
+    private static int notifierId = 0;
+    private int id;
+    private NotificationManager notificationManager;
+    private Context context;
+
+    public Notifier(Context context){
+        this.context = context;
+        notificationManager = (NotificationManager) this.context.getSystemService(Context.NOTIFICATION_SERVICE);
+        this.id = notifierId++;
+    }
+
+    /**
+     * Sends a notification to the system for testing.
+     * @param title Title of the notification
+     * @param content Text content of the notification
+     */
+    public void sendNotification(String title, String content){
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context)
+                .setSmallIcon(R.drawable.notification_template_icon_bg)
+                .setContentTitle(title)
+                .setContentText(content);
+
+        notificationManager.notify(id, builder.build());
+    }
+}
